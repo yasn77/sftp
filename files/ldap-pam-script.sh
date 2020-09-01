bf545a56e44c0357b50b222e90cee529280a5fd2
Yasser Saleemi <yassersaleemi@gmail.com>
2020-09-01 15:30:32 +0100

    Fix shellcheck errors

--- files/ldap-pam-script.sh
+++ files/ldap-pam-script.sh
@@ -4,12 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-USER_GROUP=$(id -g ${PAM_USER})
-USER_HOME="$(getent passwd ${PAM_USER} | cut -d ':' -f 6)"
+USER_GROUP=$(id -g "${PAM_USER}")
+USER_HOME=$(getent passwd "${PAM_USER}" | cut -d ':' -f 6)
 
 do_session() {
-  install -d -o root -g root -m 0755 ${USER_HOME}
-  install -d -o $PAM_USER -g $USER_GROUP -m 0700 ${USER_HOME}/sftp
+  install -d -o root -g root -m 0755 "${USER_HOME}"
+  install -d -o "${PAM_USER}" -g "${USER_GROUP}" -m 0700 "${USER_HOME}/sftp"
 }
 
 case "$PAM_TYPE" in
